1a1eb7d0c4f79ae193887ce27e67b3a3476c79d0
lore-lml <lorenzo.limoli@studenti.polito.it>
2020-06-30 12:12:43 +0200

    caltech

--- Caltech101/caltech_dataset.py
+++ Caltech101/caltech_dataset.py
@@ -1,13 +1,13 @@
-from torchvision.datasets import VisionDataset
-from sklearn.model_selection import train_test_split
-
-from PIL import Image
-
 import os
 import os.path
-import sys
-from random import randint
+import pandas as pd
+
 import numpy as np
+from PIL import Image
+from sklearn.model_selection import train_test_split
+from torch.utils.data import Dataset
+from torchvision.datasets import VisionDataset
+
 
 def pil_loader(path):
     # open path as file to avoid ResourceWarning (https://github.com/python-pillow/Pillow/issues/835)
@@ -15,25 +15,37 @@ def pil_loader(path):
         img = Image.open(f)
         return img.convert('RGB')
 
-def random_split_indices(dataset, train_size=.5):
-    validation_size = int(dataset.__len__() * (1-train_size))
-    train_indices = list(range(dataset.__len__()))
-    validation_indices = []
-    for i in range(validation_size):
-        ind = randint(0, len(train_indices))
-        validation_indices.append(train_indices.pop(ind))
 
-    return train_indices, validation_indices
+class CaltechSubset(Dataset):
+
+    def __init__(self, images, labels, transforms):
+        assert len(images) == len(labels)
+        self.images = list(images)
+        self.labels = list(labels)
+        self.transforms = transforms
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, index):
+        img, label = self.images[index], self.labels[index]
+
+        if self.transforms is not None:
+            img = self.transforms(img)
+
+        return img, label
+
+
 
 class Caltech(VisionDataset):
-    def __init__(self, root, split='train', transform=None, target_transform=None):
-        super(Caltech, self).__init__(root, transform=transform, target_transform=target_transform)
+    def __init__(self, root, src='train', transform=None, eval_transform=None):
+        super(Caltech, self).__init__(root, transform=transform)
 
         self.root = root
-        self.split = split # This defines the split you are going to use
-                           # (split files are called 'train.txt' and 'test.txt')
+        self.split = src # This defines the src you are going to use
+                           # (src files are called 'train.txt' and 'test.txt')
         self.transform = transform
-        self.target_transform = target_transform
+        self.eval_transform = eval_transform
 
         '''
         - Here you should implement the logic for reading the splits files and accessing elements
@@ -44,19 +56,24 @@ class Caltech(VisionDataset):
         - Labels should start from 0, so for Caltech you will have lables 0...100 (excluding the background class) 
         '''
 
-        split_path = "Caltech101/train.txt" if split == "train" else "Caltech101/test.txt"
+        split_path = "Caltech101/train.txt" if src == "train" else "Caltech101/test.txt"
         self.dir_images = []
-        self.labels = []
-        self.images = []
+        classes = []
+        images = []
         with open(split_path) as reader:
             for line in reader:
                 if not line.startswith("BACKGROUND_Google"):
-                    self.labels.append(line.split("/")[0])
+                    classes.append(line.split("/")[0])
                     image_path = os.path.join(self.root, line[:-1])
                     self.images.append(pil_loader(image_path))
 
         self.class_names = {k: i for i, k in enumerate(sorted(set(self.labels)))}
-        self.labels_int = [self.class_names[v] for v in self.labels]
+        labels_int = [self.class_names[v] for v in self.labels]
+
+        self.df = pd.DataFrame({
+            'image': pd.Series(list(images)),
+            'label': labels_int,
+        })
 
     def __getitem__(self, index):
         '''
@@ -70,7 +87,7 @@ class Caltech(VisionDataset):
         # Provide a way to access image and label via index
         # Image should be a PIL Image
         # label can be int
-        image, label = self.images[index], self.labels_int[index]
+        image, label = self.df.loc[index, 'image'], self.df.loc[index, 'label']
 
         # Applies preprocessing when accessing the image
         if self.transform is not None:
@@ -84,10 +101,13 @@ class Caltech(VisionDataset):
         It is mandatory, as this is used by several other components
         '''
         # Provide a way to get the length (number of elements) of the dataset
-        return len(self.labels)
+        return self.df.shape[0]
 
     def get_labels(self):
-        return np.array(self.labels_int)
+        return np.array(self.df.loc[:, 'label'])
 
-    def get_train_val_idxs(self, train_size=.5):
-        return train_test_split(np.arange(self.__len__()), train_size=train_size, stratify=self.labels_int)
+    def get_train_validation_set(self, train_size=.5):
+        train_idx, val_idx = train_test_split(np.arange(self.__len__()), train_size=train_size, stratify=self.df.loc[:, 'label'])
+        train_subset = CaltechSubset(self.df.loc[train_idx, 'image'], self.df.loc[train_idx, 'label'], self.transform)
+        val_subset = CaltechSubset(self.df.loc[val_idx, 'image'], self.df.loc[val_idx, 'label'], self.eval_transform)
+        return train_subset, val_subset
